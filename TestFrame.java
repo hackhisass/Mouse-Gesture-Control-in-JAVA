fd5bd6473cd17e4cbce39018a52b436d5b21b33e
hackhisass <uditagarwal1997@gmail.com>
2016-06-15 22:54:22 +0530

    Update TestFrame.java

--- TestFrame.java
+++ TestFrame.java
@@ -1,22 +1,6 @@
-/*
-MouseGestures - pure Java library for recognition and processing mouse gestures.
-Copyright (C) 2003-2004 Smardec
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
+//before starting,import the Mouse gesture jar file into your project
 package com.smardec.mousegestures.test;
 
 import com.smardec.mousegestures.MouseGestures;
